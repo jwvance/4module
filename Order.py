7f137a5cbf90fb969bdf04eed9fc51b5a9a4cc64
Jason <jvance@verogen.com>
2021-03-02 21:36:02 -0800

    Add comment to clarify what Orders are.

--- Order.py
+++ Order.py
@@ -1,13 +1,14 @@
 # An order is a list of food.
 # You can add to, remove from, or show the order
-Order = []
+Order = [] # List of food
+
+
 
 # Print foods in Order
 def show():
     print("Orders:")
     for food in Order: # for all the things in food
         print(" ~ " + food) #prints out all the items in a list with "~" serving as a bullet point
-
 # Add food to Order
 def add():
     newFood = input("What would you like to add to your Order?: ")
